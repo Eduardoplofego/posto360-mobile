a3e3a151c69cbc7f26c24fa9264bb526bd233368
Luiz Falqueto <lgfalqueto85@gmail.com>
2025-03-18 10:38:31 -0300

    refactor: deleting restclient not used

--- lib/core/bindings/application_bindings.dart
+++ lib/core/bindings/application_bindings.dart
@@ -1,7 +1,5 @@
 import 'package:get/get.dart';
-import 'package:posto360/core/rest_client/dio/dio_client.dart';
 import 'package:posto360/core/rest_client/posto_rest_client.dart';
-import 'package:posto360/core/rest_client/rest_client.dart';
 import 'package:posto360/core/services/auth_service.dart';
 
 class ApplicationBindings implements Bindings {
