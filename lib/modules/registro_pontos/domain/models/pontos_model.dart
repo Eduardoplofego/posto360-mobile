98370bc8f59fb80df6ebbb1096b5b0bda3c66567
Luiz Falqueto <lgfalqueto85@gmail.com>
2026-03-01 11:28:00 -0300

    cleanup: codigo comentado

--- lib/modules/registro_pontos/domain/models/pontos_model.dart
+++ lib/modules/registro_pontos/domain/models/pontos_model.dart
@@ -35,30 +35,6 @@ class PontosModel {
     final isInFormat = regex.hasMatch(totalHour);
 
     return isInFormat ? totalHour : '-';
-    // Duration parseTime(String? time) {
-    //   if (time == null) return Duration.zero;
-
-    //   final match = RegExp(r'(\d{1,2}):(\d{2})').firstMatch(time);
-    //   if (match == null) return Duration.zero;
-
-    //   final hour = int.parse(match.group(1)!);
-    //   final minute = int.parse(match.group(2)!);
-
-    //   return Duration(hours: hour, minutes: minute);
-    // }
-
-    // final entrada1 = parseTime(pontos[0]);
-    // final saida1 = parseTime(pontos[1]);
-    // final entrada2 = parseTime(pontos[2]);
-    // final saida2 = parseTime(pontos[3]);
-
-    // final total = (saida1 - entrada1) + (saida2 - entrada2);
-
-    // final hours = total.inHours;
-    // final minutes = total.inMinutes.remainder(60);
-
-    // return '${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}';
-    // return "8h";
   }
 
   Map<String, dynamic> toMap() {
